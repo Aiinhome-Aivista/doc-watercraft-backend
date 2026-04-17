73a154ec6a15f4a5efd18cd7b231a10c7e6bc190
Souvik1502 <souvikkarmakar1502@gmail.com>
2026-04-17 13:31:52 +0530

    BILLING ADDED

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from controllers.gate_controller import (
 )
 from controllers.partymasters_controller import (get_partymaster, get_partymasters, create_partymaster, update_partymaster,delete_partymaster)
 
-from controllers.billing_controller import (get_vessels_for_billing)
+from controllers.billing_controller import (get_vessels_for_billing,)
 import os
 
 
@@ -108,13 +108,13 @@ def route_rates_by_vessel(vessel_id):return get_rates_by_vessel(vessel_id)
 @token_required
 def route_billing_vessels():return get_vessels_for_billing()
 
-# @app.route(API + '/rates/<int:vessel_id>/<int:rate_id>', methods=['POST'])
-# @token_required
-# def route_vessel_billing(vessel_id, rate_id): return update_rate(vessel_id, rate_id)
+@app.route(API + '/rates/<int:vessel_id>/<int:rate_id>', methods=['POST'])
+@token_required
+def route_vessel_billing(vessel_id, rate_id): return update_rate(vessel_id, rate_id)
 
-# @app.route(API + '/mis/report', methods=['GET'])
-# @token_required
-# def route_mis_report(): return get_mis_report()
+@app.route(API + '/mis/report', methods=['GET'])
+@token_required
+def route_mis_report(): return get_mis_report()
 
 # ─── Gate Entry Routes ──────────────────────────────────────────
 @app.route(API + '/gate-entries', methods=['GET'])
